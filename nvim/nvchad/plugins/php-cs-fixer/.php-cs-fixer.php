563cd07cb08e32f32a4599a1d612ad6772873926
Mathias Arlaud <mathias.arlaud@gmail.com>
2022-11-03 10:01:44 +0100

    [nvim] update php-cs-fixer rules

--- nvim/nvchad/plugins/php-cs-fixer/.php-cs-fixer.php
+++ nvim/nvchad/plugins/php-cs-fixer/.php-cs-fixer.php
@@ -10,12 +10,13 @@ return (new PhpCsFixer\Config())
         '@PSR12' => true,
         '@PSR12:risky' => true,
         'trailing_comma_in_multiline' => true,
+        'method_chaining_indentation' => true,
+        'array_indentation' => true,
         'ordered_imports' => [
             'imports_order' => ['class', 'function', 'const'],
             'sort_algorithm' => 'alpha',
         ],
-	'declare_strict_types' => true,
-
+        'declare_strict_types' => true,
     ])
     ->setRiskyAllowed(true)
 ;
